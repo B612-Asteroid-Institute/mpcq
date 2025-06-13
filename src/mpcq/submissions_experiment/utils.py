23df9de0f67e88129761dd3d59a41ccf04b6124e
Joachim Moeyens <moeyensj@uw.edu>
2025-06-13 12:02:25 -0700

    Add prepare_submission_tables

--- src/mpcq/submissions_experiment/utils.py
+++ src/mpcq/submissions_experiment/utils.py
@@ -8,6 +8,13 @@ import quivr as qv
 from adam_core.observations import ADESObservations, SourceCatalog
 from adam_core.time import Timestamp
 
+from .types import (
+    AssociationCandidates,
+    AssociationMembers,
+    DiscoveryCandidateMembers,
+    DiscoveryCandidates,
+)
+
 
 def round_to_nearest_millisecond(t: datetime.datetime) -> datetime.datetime:
     microseconds = np.ceil(t.microsecond / 1000).astype(int) * 1000
@@ -126,7 +133,130 @@ def candidates_to_ades(
     )
 
     ades_tables = []
-    for chunk in split_into_max_size(ades, max_observations_per_table):
+    for chunk in split_into_max_size(ades, "trkSub", max_observations_per_table):
         ades_tables.append(chunk)
 
     return ades_tables
+
+
+def prepare_submission_tables(
+    source_catalog: SourceCatalog,
+    discovery_candidates: Optional[DiscoveryCandidates] = None,
+    discovery_candidate_members: Optional[DiscoveryCandidateMembers] = None,
+    association_candidates: Optional[AssociationCandidates] = None,
+    association_members: Optional[AssociationMembers] = None,
+    max_observations_per_file: Optional[int] = 10000,
+) -> tuple[List[ADESObservations], List[ADESObservations]]:
+    """
+    Generate ADESObservation tables for the given candidates and their members.
+
+    Parameters
+    ----------
+    source_catalog : SourceCatalog
+        The source catalog.
+    discovery_candidates : Optional[DiscoveryCandidates], optional
+        The discovery candidates. These are objects believed to be newly observed.
+    discovery_candidate_members : Optional[DiscoveryCandidateMembers], optional
+        The observation members of the discovery candidates.
+    association_candidates : Optional[AssociationCandidates], optional
+        The association candidates. These are objects believed to be associated with a known object.
+    association_members : Optional[AssociationMembers], optional
+        The observation members of the association candidates.
+    max_observations_per_file : Optional[int], optional
+        The maximum number of observations per file, by default 100000.
+
+    Returns
+    -------
+    discovery_ades, association_ades : tuple[List[ADESObservations], List[ADESObservations]]
+        The ADESObservations for the discovery and association candidates.
+    """
+    if discovery_candidates is None and association_candidates is None:
+        raise ValueError(
+            "At least one of discovery_candidates or association_candidates must be provided."
+        )
+
+    if discovery_candidates is not None:
+        if discovery_candidate_members is None:
+            raise ValueError(
+                "discovery_candidate_members must be provided if discovery_candidates is provided."
+            )
+
+        if not pc.all(
+            pc.is_in(discovery_candidates.trksub, discovery_candidate_members.trksub)
+        ).as_py():
+            raise ValueError(
+                "All trksubs in discovery_candidates must be present in discovery_candidate_members."
+            )
+
+        if not pc.all(
+            pc.is_in(discovery_candidate_members.trksub, discovery_candidates.trksub)
+        ).as_py():
+            raise ValueError(
+                "All trksubs in discovery_candidate_members must be present in discovery_candidates."
+            )
+
+        if not pc.all(
+            pc.is_in(discovery_candidate_members.obssubid, source_catalog.id)
+        ).as_py():
+            raise ValueError(
+                "All obssubids in discovery_candidate_members must be present in source_catalog."
+            )
+
+    if association_candidates is not None:
+        if association_members is None:
+            raise ValueError(
+                "association_members must be provided if association_candidates is provided."
+            )
+
+        if not pc.all(
+            pc.is_in(association_candidates.trksub, association_members.trksub)
+        ).as_py():
+            raise ValueError(
+                "All trksubs in association_candidates must be present in association_members."
+            )
+
+        if not pc.all(
+            pc.is_in(association_members.trksub, association_candidates.trksub)
+        ).as_py():
+            raise ValueError(
+                "All trksubs in association_members must be present in association_candidates."
+            )
+
+        if not pc.all(
+            pc.is_in(association_members.obssubid, source_catalog.id)
+        ).as_py():
+            raise ValueError(
+                "All obssubids in association_members must be present in source_catalog."
+            )
+
+    # Create ADES Observations for discovery candidates
+    if discovery_candidates is not None:
+        if len(discovery_candidates) == 0:
+            discovery_ades = [ADESObservations.empty()]
+        else:
+            discovery_ades = candidates_to_ades(
+                discovery_candidates,
+                discovery_candidate_members,
+                source_catalog,
+                max_observations_per_table=max_observations_per_file,
+            )
+
+    else:
+        discovery_ades = [ADESObservations.empty()]
+
+    # Create ADES Observations for association candidates
+    if association_candidates is not None:
+        if len(association_candidates) == 0:
+            association_ades = [ADESObservations.empty()]
+        else:
+            association_ades = candidates_to_ades(
+                association_candidates,
+                association_members,
+                source_catalog,
+                max_observations_per_table=max_observations_per_file,
+            )
+
+    else:
+        association_ades = [ADESObservations.empty()]
+
+    return discovery_ades, association_ades
