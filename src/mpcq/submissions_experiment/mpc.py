8b3f797ffecd1490a4e9f0019fad44b8d472ab1c
Joachim Moeyens <moeyensj@uw.edu>
2025-06-25 11:30:26 -0700

    Add MPCSubmissionOfficialClient

--- src/mpcq/submissions_experiment/mpc.py
+++ src/mpcq/submissions_experiment/mpc.py
@@ -1,4 +1,10 @@
 from abc import ABC, abstractmethod
+from datetime import datetime, timezone
+from typing import List, Optional, Tuple
+
+import requests
+
+from .wamo import WAMOResults
 
 
 class MPCSubmissionClient(ABC):
@@ -14,3 +20,100 @@ class MPCSubmissionClient(ABC):
     @abstractmethod
     def query_wamo():
         pass
+
+
+class MPCSubmissionOfficialClient(MPCSubmissionClient):
+
+    def submit_ades(
+        self,
+        file: str,
+        email: str,
+        comment: str,
+        object_type: Optional[str] = None,
+    ) -> Tuple[str, datetime]:
+        """
+        Submit a PSV file to the MPC submission upload form.
+
+        Parameters
+        ----------
+        file : str
+            Path to the PSV file to submit.
+        email : str
+            Email address of the submitter.
+        comment : str
+            Comment to include in the submission (this is the acknowledgement contained
+            in emailed receipt)
+        object_type : str, optional
+            Type of object being submitted. Default is "Unclassified".
+            Options are: "Unclassified", "Comet", "Asteroid", "Dwarf Planet", "Satellite", "Other".
+            This is used to categorize the submission in the MPC database. If not provided, "Unclassified" is used.
+            See https://minorplanetcenter.net/submit_psv for more details.
+
+        Returns
+        -------
+        mpc_submission_id : str
+            The MPC submission ID.
+        submission_time : datetime
+            The time the submission was made.
+
+        Raises
+        ------
+        ValueError
+            If the submission ID is not found.
+            If the submission fails.
+        """
+        if object_type is None:
+            object_type = "Unclassified"
+
+        url = "https://minorplanetcenter.net/submit_psv"
+
+        files = {
+            "ack": (None, comment),
+            "ac2": (None, email),
+            "source": (None, open(file, "rb")),
+        }
+
+        submission_time = datetime.now().astimezone(timezone.utc)
+        response = requests.post(url, files=files)
+
+        if response.status_code == 200:
+            idx = response.text.find("Submission ID is")
+            if idx != 1:
+                mpc_submission_id = response.text[idx + 17 : idx + 17 + 32]
+                return mpc_submission_id, submission_time
+            else:
+                raise ValueError(f"Submission ID not found: {response.text}")
+        else:
+            raise ValueError(
+                f"Submission failed: {response.text} (status code {response.status_code})"
+            )
+
+    def submit_identifications():
+        pass
+
+    def query_wamo(self, requested_values: List[str]) -> WAMOResults:
+        """
+        Query the WAMO API for the requested values.
+
+        These may take the form of:
+            (trksub, stn), ...
+            obsid, ...
+            obs80, ...
+            submission_block_id, ...
+
+
+        Parameters
+        ----------
+        requested_values : List[str]
+            The values to query the WAMO API for.
+
+        Returns
+        -------
+        WAMOResults
+            The results of the WAMO API query.
+        """
+        url = "https://data.minorplanetcenter.net/api/wamo"
+        result = requests.get(url, json=requested_values)
+        observations = result.json()
+
+        return WAMOResults.from_json(observations)
