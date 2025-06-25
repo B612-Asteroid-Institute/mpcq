410d7314623ccaf99132a4d88a0d15d2069db45e
Joachim Moeyens <moeyensj@uw.edu>
2025-06-24 18:32:36 -0700

    Implement PostgresMPCClient query_primary_objects and query_submission_history, readd alternate designation joins

--- src/mpcq/client/postgres.py
+++ src/mpcq/client/postgres.py
@@ -14,6 +14,7 @@ from ..orbits import MPCOrbits, MPCPrimaryObjects
 from ..submissions import (
     MPCSubmissionHistory,
     MPCSubmissionResults,
+    infer_submission_time,
 )
 from .client import MPCClient
 
@@ -26,11 +27,11 @@ class PostgresMPCClient(MPCClient):
 
     def __init__(
         self,
+        host: str,
         database: str,
         user: str,
-        host: str,
-        port: int,
         password: str,
+        port: int = 5432,
         schema: str = "public",
         **kwargs: Any,
     ) -> None:
@@ -108,7 +109,10 @@ class PostgresMPCClient(MPCClient):
         )
         SELECT DISTINCT
             rp.provid AS requested_provid,
-            obs_sbn.provid AS primary_designation,
+            CASE 
+                WHEN ni.permid IS NOT NULL THEN ni.permid 
+                ELSE ci.unpacked_primary_provisional_designation
+            END AS primary_designation,
             obs_sbn.obsid, 
             obs_sbn.trksub, 
             obs_sbn.permid, 
@@ -131,8 +135,16 @@ class PostgresMPCClient(MPCClient):
             obs_sbn.astcat,
             obs_sbn.mode
         FROM requested_provids AS rp
+        LEFT JOIN current_identifications AS ci
+            ON ci.unpacked_secondary_provisional_designation = rp.provid
+        LEFT JOIN current_identifications AS ci_alt
+            ON ci.unpacked_primary_provisional_designation = ci_alt.unpacked_primary_provisional_designation
+        LEFT JOIN numbered_identifications AS ni
+            ON ci.unpacked_primary_provisional_designation = ni.unpacked_primary_provisional_designation
         LEFT JOIN obs_sbn AS obs_sbn
-            ON rp.provid = obs_sbn.provid
+            ON ci.unpacked_primary_provisional_designation = obs_sbn.provid
+            OR ci_alt.unpacked_secondary_provisional_designation = obs_sbn.provid
+            OR ni.permid = obs_sbn.permid
         ORDER BY requested_provid ASC, obs_sbn.obstime ASC
         """
         with self.conn.cursor() as cursor:
@@ -204,7 +216,10 @@ class PostgresMPCClient(MPCClient):
         )
         SELECT DISTINCT 
             rp.provid AS requested_provid,
-            mpc_orbits.unpacked_primary_provisional_designation AS primary_designation,
+            CASE
+                WHEN ni.permid IS NOT NULL THEN ni.permid
+                ELSE ci.unpacked_primary_provisional_designation
+            END AS primary_designation,
             mpc_orbits.id, 
             mpc_orbits.unpacked_primary_provisional_designation AS provid, 
             mpc_orbits.epoch_mjd,
@@ -228,8 +243,14 @@ class PostgresMPCClient(MPCClient):
             mpc_orbits.created_at,
             mpc_orbits.updated_at
         FROM requested_provids AS rp
+        LEFT JOIN current_identifications AS ci
+            ON ci.unpacked_secondary_provisional_designation = rp.provid
+        LEFT JOIN current_identifications AS ci_alt
+            ON ci.unpacked_primary_provisional_designation = ci_alt.unpacked_primary_provisional_designation
+        LEFT JOIN numbered_identifications AS ni
+            ON ci.unpacked_primary_provisional_designation = ni.unpacked_primary_provisional_designation
         LEFT JOIN mpc_orbits AS mpc_orbits
-            ON mpc_orbits.unpacked_primary_provisional_designation = rp.provid
+            ON ci.unpacked_primary_provisional_designation = mpc_orbits.unpacked_primary_provisional_designation
         ORDER BY 
             requested_provid ASC,
             mpc_orbits.epoch_mjd ASC
@@ -289,13 +310,234 @@ class PostgresMPCClient(MPCClient):
     def query_submission_results(
         self, submission_ids: List[str]
     ) -> MPCSubmissionResults:
-        pass
+        """
+        Query for observation status and mapping (observation ID to trksub, provid, etc.) for a
+        given list of submission IDs.
+
+        Parameters
+        ----------
+        submission_ids : List[str]
+            List of submission IDs to query.
+
+        Returns
+        -------
+        submission_info : MPCSubmissionResults
+            The observation status and mapping for the given submission IDs.
+        """
+        query = f"""
+        WITH requested_submission_ids AS (
+            SELECT submission_id
+            FROM UNNEST(ARRAY[{_to_postgres_string_array(submission_ids)}]) AS submission_id
+        )
+        SELECT DISTINCT
+            sb.submission_id AS requested_submission_id,
+            obs_sbn.obsid,
+            obs_sbn.obssubid, 
+            obs_sbn.trksub, 
+            CASE 
+                WHEN ni.permid IS NOT NULL THEN ni.permid 
+                ELSE ci.unpacked_primary_provisional_designation
+            END AS primary_designation,
+            obs_sbn.permid, 
+            obs_sbn.provid, 
+            obs_sbn.submission_id, 
+            obs_sbn.status
+        FROM requested_submission_ids AS sb
+        LEFT JOIN obs_sbn AS obs_sbn
+            ON sb.submission_id = obs_sbn.submission_id
+        LEFT JOIN current_identifications AS ci
+            ON ci.unpacked_secondary_provisional_designation = obs_sbn.provid
+            OR ci.unpacked_primary_provisional_designation = obs_sbn.provid
+        LEFT JOIN numbered_identifications AS ni
+            ON obs_sbn.permid = ni.permid
+        ORDER BY requested_submission_id ASC, obs_sbn.obsid ASC
+        """
+        with self.conn.cursor() as cursor:
+            cursor.execute(query)
+            table = cursor.fetch_arrow_table().combine_chunks()
+
+        return MPCSubmissionResults.from_kwargs(
+            requested_submission_id=pc.cast(
+                table.column("requested_submission_id"), pa.large_string()
+            ),
+            obsid=pc.cast(table.column("obsid"), pa.large_string()),
+            obssubid=pc.cast(table.column("obssubid"), pa.large_string()),
+            trksub=pc.cast(table.column("trksub"), pa.large_string()),
+            primary_designation=pc.cast(
+                table.column("primary_designation"), pa.large_string()
+            ),
+            provid=pc.cast(table.column("provid"), pa.large_string()),
+            submission_id=pc.cast(table.column("submission_id"), pa.large_string()),
+            status=pc.cast(table.column("status"), pa.large_string()),
+        )
 
     def query_submission_history(self, provids: List[str]) -> MPCSubmissionHistory:
-        pass
+        """
+        Query for submission history for a given list of provisional designations.
+
+        Parameters
+        ----------
+        provids : List[str]
+            List of provisional designations to query.
+
+        Returns
+        -------
+        submission_history : MPCSubmissionHistory
+            The submission history for the given provisional designations.
+        """
+        query = f"""
+        WITH requested_provids AS (
+            SELECT provid
+            FROM UNNEST(ARRAY[{_to_postgres_string_array(provids)}]) AS provid
+        )
+        SELECT DISTINCT
+            rp.provid AS requested_provid,
+            CASE 
+                WHEN ni.permid IS NOT NULL THEN ni.permid 
+                ELSE ci.unpacked_primary_provisional_designation
+            END AS primary_designation,
+            obs_sbn.obsid, 
+            obs_sbn.obstime,
+            obs_sbn.submission_id
+        FROM requested_provids AS rp 
+        LEFT JOIN current_identifications AS ci
+            ON ci.unpacked_secondary_provisional_designation = rp.provid
+        LEFT JOIN current_identifications AS ci_alt
+            ON ci.unpacked_primary_provisional_designation = ci_alt.unpacked_primary_provisional_designation
+        LEFT JOIN numbered_identifications AS ni
+            ON ci.unpacked_primary_provisional_designation = ni.unpacked_primary_provisional_designation
+        LEFT JOIN obs_sbn AS obs_sbn
+            ON ci.unpacked_primary_provisional_designation = obs_sbn.provid
+            OR ci_alt.unpacked_secondary_provisional_designation = obs_sbn.provid
+            OR ni.permid = obs_sbn.permid
+        ORDER BY requested_provid ASC, obs_sbn.obstime ASC
+        """
+        with self.conn.cursor() as cursor:
+            cursor.execute(query)
+            table = cursor.fetch_arrow_table().combine_chunks()
+
+        table = (
+            table.group_by(["requested_provid", "primary_designation", "submission_id"])
+            .aggregate(
+                [("obsid", "count_distinct"), ("obstime", "min"), ("obstime", "max")]
+            )
+            .sort_by(
+                [("primary_designation", "ascending"), ("submission_id", "ascending")]
+            )
+            .rename_columns(
+                [
+                    "requested_provid",
+                    "primary_designation",
+                    "submission_id",
+                    "num_obs",
+                    "first_obs_time",
+                    "last_obs_time",
+                ]
+            )
+        )
+
+        # Create array that tracks the index of each row
+        table = table.append_column("idx", pa.array(np.arange(len(table))))
+
+        # Find the first and last index of each group (first and last submission)
+        # and append boolean columns to the table
+        first_last_idx = table.group_by(
+            ["primary_designation"], use_threads=False
+        ).aggregate([("idx", "first"), ("idx", "last")])
+        first = np.zeros(len(table), dtype=bool)
+        last = np.zeros(len(table), dtype=bool)
+        first[first_last_idx["idx_first"].to_numpy(zero_copy_only=False)] = True
+        last[first_last_idx["idx_last"].to_numpy(zero_copy_only=False)] = True
+        table = table.append_column("first_submission", pa.array(first))
+        table = table.append_column("last_submission", pa.array(last))
+
+        # Calculate the arc length of each submission
+        start_times = Time(
+            table["first_obs_time"].to_numpy(zero_copy_only=False), scale="utc"
+        )
+        end_times = Time(
+            table["last_obs_time"].to_numpy(zero_copy_only=False), scale="utc"
+        )
+        arc_length = end_times.utc.mjd - start_times.utc.mjd
+
+        return MPCSubmissionHistory.from_kwargs(
+            requested_provid=table["requested_provid"],
+            primary_designation=table["primary_designation"],
+            submission_id=table["submission_id"],
+            submission_time=infer_submission_time(
+                table["submission_id"].to_numpy(zero_copy_only=False),
+                end_times.utc.isot,
+            ),
+            first_submission=table["first_submission"],
+            last_submission=table["last_submission"],
+            num_obs=table["num_obs"],
+            first_obs_time=Timestamp.from_astropy(start_times),
+            last_obs_time=Timestamp.from_astropy(end_times),
+            arc_length=arc_length,
+        ).sort_by(["requested_provid", "submission_id"])
 
     def query_primary_objects(self, provids: List[str]) -> MPCPrimaryObjects:
-        pass
+        """
+        Query the MPC database for the primary objects and associated data for the given
+        provisional designations.
+
+        Parameters
+        ----------
+        provids : List[str]
+            List of provisional designations to query.
+
+        Returns
+        -------
+        primary_objects : MPCPrimaryObjects
+            The primary objects and associated data for the given provisional designations.
+        """
+        query = f"""
+        WITH requested_provids AS (
+            SELECT provid
+            FROM UNNEST(ARRAY[{_to_postgres_string_array(provids)}]) AS provid
+        )
+        SELECT DISTINCT
+            rp.provid AS requested_provid,
+            CASE 
+                WHEN ni.permid IS NOT NULL THEN ni.permid 
+                ELSE ci.unpacked_primary_provisional_designation
+            END AS primary_designation,
+            po.unpacked_primary_provisional_designation as provid, 
+            po.created_at, 
+            po.updated_at
+        FROM requested_provids AS rp
+        LEFT JOIN current_identifications AS ci
+            ON ci.unpacked_secondary_provisional_designation = rp.provid
+        LEFT JOIN current_identifications AS ci_alt
+            ON ci.unpacked_primary_provisional_designation = ci_alt.unpacked_primary_provisional_designation
+        LEFT JOIN numbered_identifications AS ni
+            ON ci.unpacked_primary_provisional_designation = ni.unpacked_primary_provisional_designation
+        LEFT JOIN primary_objects AS po
+            ON ci.unpacked_primary_provisional_designation = po.unpacked_primary_provisional_designation
+        ORDER BY requested_provid ASC
+        """
+        with self.conn.cursor() as cursor:
+            cursor.execute(query)
+            table = cursor.fetch_arrow_table().combine_chunks()
+
+        created_at = Time(
+            table["created_at"].to_numpy(zero_copy_only=False),
+            format="datetime64",
+            scale="utc",
+        )
+        updated_at = Time(
+            table["updated_at"].to_numpy(zero_copy_only=False),
+            format="datetime64",
+            scale="utc",
+        )
+
+        return MPCPrimaryObjects.from_kwargs(
+            requested_provid=table["requested_provid"],
+            primary_designation=table["primary_designation"],
+            provid=table["provid"],
+            created_at=Timestamp.from_astropy(created_at),
+            updated_at=Timestamp.from_astropy(updated_at),
+        )
 
     def cross_match_observations(
         self,
