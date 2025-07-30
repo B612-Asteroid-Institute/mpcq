e0cbf3e576bd01bef753203fd155fa93d0eb0509
Joachim Moeyens <moeyensj@uw.edu>
2025-07-30 14:02:01 -0700

    Add MPCSandboxSubmissionClient

--- src/mpcq/submissions_experiment/mpc.py
+++ src/mpcq/submissions_experiment/mpc.py
@@ -1,9 +1,10 @@
+import logging
+import os
 from abc import ABC, abstractmethod
 from datetime import datetime, timezone
 from typing import List, Optional, Tuple
 
 import requests
-import logging
 
 from .wamo import WAMOResults
 
@@ -23,7 +24,7 @@ class MPCSubmissionClient(ABC):
         pass
 
 
-class MPCSubmissionOfficialClient(MPCSubmissionClient):
+class MPCOfficialSubmissionClient(MPCSubmissionClient):
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
@@ -122,3 +123,75 @@ class MPCSubmissionOfficialClient(MPCSubmissionClient):
         observations = result.json()
 
         return WAMOResults.from_json(observations)
+
+
+class MPCSandboxSubmissionClient(MPCSubmissionClient):
+
+    def __init__(self, url: str):
+        self.logger = logging.getLogger(__name__)
+        self.url = url
+
+    def submit_ades(
+        self,
+        file: str,
+        email: str,
+        comment: str,
+        object_type: str = "Unclassified",
+    ) -> Tuple[str, datetime]:
+        """
+        Submit a PSV file to the MPC submission upload form.
+
+        Parameters
+        ----------
+        file : str
+            Path to the PSV file to submit.
+        email : str
+            Email address of the submitter.
+        comment : str
+            Comment to include in the submission (this is the acknowledgement contained
+            in emailed receipt)
+        object_type : str, optional
+            Type of object being submitted. Default is "Unclassified".
+            Options are: "Unclassified", "Comet", "Asteroid", "Dwarf Planet", "Satellite", "Other".
+            This is used to categorize the submission in the MPC database. If not provided, "Unclassified" is used.
+            See https://minorplanetcenter.net/submit_psv for more details.
+
+        Returns
+        -------
+        mpc_submission_id : str
+            The MPC submission ID.
+        submission_time : datetime
+            The time the submission was made.
+
+        Raises
+        ------
+        ValueError
+            If the submission ID is not found.
+            If the submission fails.
+        """
+        files = {
+            "ack": (None, comment),
+            "ac2": (None, email),
+            "source": (None, open(file, "rb")),
+        }
+        submission_time = datetime.now(timezone.utc)
+        response = requests.post(os.path.join(self.url, "psv/"), files=files)
+        self.logger.info(f"Submission response: {response.text}")
+
+        if response.status_code == 200:
+            idx = response.text.find("MPC submission ID : ")
+            if idx != -1:
+                mpc_submission_id = response.text[idx + 28 : idx + 28 + 32]
+                return mpc_submission_id, submission_time
+            else:
+                raise ValueError(f"Submission ID not found: {response.text}")
+        else:
+            raise ValueError(
+                f"Submission failed: {response.text} (status code {response.status_code})"
+            )
+
+    def submit_identifications(self):
+        pass
+
+    def query_wamo(self, requested_values: List[str]) -> WAMOResults:
+        pass
